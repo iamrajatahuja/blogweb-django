e82013ee648605ccf7f9a557035cb9d3669e2bf4
Rajat Ahuja <rajatahujaindia@gmail.com>
2026-06-29 15:49:44 +0530

    Adding models and using admin

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from .models import Post, Author, Tag
+
+#To tweak and display the admin Posts page and prepopulate the slug field while adding post, use the same variable names-list_display,list_filter
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'date')
+    list_filter = ('author', 'tags', 'date')
+    prepopulated_fields = {'slug': ('title',)}
+
 # Register your models here.
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Author)
+admin.site.register(Tag)
